0a1e765f75eb48791e92912544257300d5d77d9e
Yoon321 <tkdwls734@gmail.com>
2024-10-24 04:06:03 +0900

    [chore] #17 - 상품, 아이템 임시 데이터 추가

---
+++ product/src/main/resources/data.sql
@@ -0,0 +1,52 @@
+INSERT INTO products (name, description, price, active, created_at, updated_at) VALUES
+('Product 1', 'Description for Product 1', 10000, true, NOW(), NOW()),
+('Product 2', 'Description for Product 2', 20000, true, NOW(), NOW()),
+('Product 3', 'Description for Product 3', 30000, true, NOW(), NOW()),
+('Product 4', 'Description for Product 4', 40000, true, NOW(), NOW()),
+( 'Product 5', 'Description for Product 5', 50000, true, NOW(), NOW()),
+( 'Product 6', 'Description for Product 6', 60000, true, NOW(), NOW()),
+( 'Product 7', 'Description for Product 7', 70000, true, NOW(), NOW()),
+( 'Product 8', 'Description for Product 8', 80000, true, NOW(), NOW()),
+( 'Product 9', 'Description for Product 9', 90000, true, NOW(), NOW()),
+( 'Product 10', 'Description for Product 10', 100000, true, NOW(), NOW()),
+( 'Product 11', 'Description for Product 11', 110000, true, NOW(), NOW()),
+( 'Product 12', 'Description for Product 12', 120000, true, NOW(), NOW()),
+( 'Product 13', 'Description for Product 13', 130000, true, NOW(), NOW()),
+( 'Product 14', 'Description for Product 14', 140000, true, NOW(), NOW()),
+( 'Product 15', 'Description for Product 15', 150000, true, NOW(), NOW()),
+( 'Product 16', 'Description for Product 16', 160000, true, NOW(), NOW()),
+( 'Product 17', 'Description for Product 17', 170000, true, NOW(), NOW()),
+( 'Product 18', 'Description for Product 18', 180000, true, NOW(), NOW()),
+( 'Product 19', 'Description for Product 19', 190000, true, NOW(), NOW()),
+( 'Product 20', 'Description for Product 20', 200000, true, NOW(), NOW()),
+( 'Product 21', 'Description for Product 21', 210000, true, NOW(), NOW()),
+( 'Product 22', 'Description for Product 22', 220000, true, NOW(), NOW()),
+( 'Product 23', 'Description for Product 23', 230000, true, NOW(), NOW()),
+( 'Product 24', 'Description for Product 24', 240000, true, NOW(), NOW()),
+( 'Product 25', 'Description for Product 25', 250000, true, NOW(), NOW()),
+( 'Product 26', 'Description for Product 26', 260000, true, NOW(), NOW()),
+( 'Product 27', 'Description for Product 27', 270000, true, NOW(), NOW()),
+( 'Product 28', 'Description for Product 28', 280000, true, NOW(), NOW()),
+( 'Product 29', 'Description for Product 29', 290000, true, NOW(), NOW()),
+( 'Product 30', 'Description for Product 30', 300000, true, NOW(), NOW());
+
+
+INSERT INTO items ( color, size, stock_quantity, price, product_id, created_at, updated_at) VALUES
+( 'Red', 100, 10, 1000, 1, NOW(), NOW()),
+( 'Blue', 100, 15, 1200, 1, NOW(), NOW()),
+( 'Green', 105, 5, 1100, 1, NOW(), NOW()),
+( 'Yellow', 105, 20, 1150, 1, NOW(), NOW()),
+( 'Black', 110, 25, 1300, 1, NOW(), NOW()),
+
+( 'Red', 100, 12, 1000, 2, NOW(), NOW()),
+( 'Blue', 100, 14, 1200, 2, NOW(), NOW()),
+( 'Green', 105, 8, 1100, 2, NOW(), NOW()),
+( 'Yellow', 105, 18, 1150, 2, NOW(), NOW()),
+( 'Black', 110, 30, 1300, 2, NOW(), NOW()),
+
+
+( 'Red', 100, 11, 1000, 3, NOW(), NOW()),
+( 'Blue', 100, 16, 1200, 3, NOW(), NOW()),
+( 'Green', 105, 6, 1100, 3, NOW(), NOW()),
+( 'Yellow', 105, 22, 1150, 3, NOW(), NOW()),
+('Black', 110, 28, 1300, 3, NOW(), NOW());
