189c6a6eda1c252ff34105e0f62daa57a0499e88
Yoon321 <tkdwls734@gmail.com>
2024-10-28 20:22:53 +0900

    [feat] #21- gateway 서버 추가 및 라우팅 설정

---
+++ gateway-server/src/main/java/com/hanghae/gatewayserver/config/CustomRoute.java
@@ -0,0 +1,23 @@
+package com.hanghae.gatewayserver.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomRoute {
+
+    @Bean
+    public RouteLocator cRoute(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+            .route(p -> p.path("/api/users/**")
+                .uri("lb://user"))
+            .route(p-> p.path("/api/auth/**")
+                .uri("lb://user"))
+            .route(p-> p.path("/api/products/**")
+                .uri("lb://product"))
+            .build();
+    }
+}
