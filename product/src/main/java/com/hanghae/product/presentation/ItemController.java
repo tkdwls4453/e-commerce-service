11ad21cbedecb30c6b7b23ae5368e7c067a5e057
Yoon321 <tkdwls734@gmail.com>
2024-10-30 00:04:04 +0900

    [fix] #20 - 상품, 아이템 정보 조회 api 수정
    GET 호출 시 RequestBody 를 사용하면 Feign Client 에서 POST 로 변경해서 보내는 문제가 있음

--- product/src/main/java/com/hanghae/product/presentation/ItemController.java
+++ product/src/main/java/com/hanghae/product/presentation/ItemController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -33,9 +34,9 @@ public class ItemController {
 
     @GetMapping
     public CustomResponse<List<ItemProductResponse>> getItemProducts(
-        @RequestBody GetItemProductRequest request
+        @RequestParam("itemIds") List<Long> itemIds
     ) {
-        List<ItemProductDto> itemProductDtos = itemService.getItemProducts(request.itemIds());
+        List<ItemProductDto> itemProductDtos = itemService.getItemProducts(itemIds);
 
         List<ItemProductResponse> result = itemProductDtos.stream().map(
                 ItemDtoMapper::toItemProductResponse
