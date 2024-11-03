07c52023c214dd0b8ec1f3352688069f497aae24
Yoon321 <tkdwls734@gmail.com>
2024-11-04 02:24:13 +0900

    [feat] #26 - 상품 + 아이템 조회 시  아이템 Id 도 함께 조회하도록 수정

--- product/src/main/java/com/hanghae/product/presentation/dto/response/ItemProductResponse.java
+++ product/src/main/java/com/hanghae/product/presentation/dto/response/ItemProductResponse.java
@@ -4,11 +4,13 @@ import lombok.Builder;
 
 @Builder
 public record ItemProductResponse(
+    Long itemId,
     String productName,
     String description,
     String color,
     Integer size,
-    Integer price
+    Integer price,
+    Integer stockQuantity
 ) {
 
 }
