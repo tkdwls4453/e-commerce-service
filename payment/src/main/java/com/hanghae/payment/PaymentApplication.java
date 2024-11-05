5e79fcf4afedbd7feffc7a793569e8fd98d06a70
Yoon321 <tkdwls734@gmail.com>
2024-11-05 09:40:35 +0900

    [feat] #28 - payment 프로젝트 생성
    - 유레카 등록
    - API GATEWAY 라우팅 연결 확인

---
+++ payment/src/main/java/com/hanghae/payment/PaymentApplication.java
@@ -0,0 +1,18 @@
+package com.hanghae.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.hanghae.payment", "com.hanghae.common"})
+public class PaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+
+}
